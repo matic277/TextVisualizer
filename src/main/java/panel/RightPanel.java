a74af65ad718003faf39960dbe92c178e502abb0
matic277 <matic277@gmail.com>
2021-05-21 12:18:12 +0200

    Functioning left panel

--- src/main/java/panel/RightPanel.java
+++ src/main/java/panel/RightPanel.java
@@ -6,10 +6,10 @@ import main.Utils;
 import word.AbsWord;
 
 import javax.swing.*;
+import javax.swing.border.StrokeBorder;
 import java.awt.*;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -18,9 +18,17 @@ public class RightPanel extends JScrollPane {
     BottomPanel parent;
     
     JPanel mainPanel;
-    JPanel sentencePanel;
-    JPanel wordStatsPanel;
-    JPanel wordStatsContentPanel;
+        // NORTH
+        JPanel mainSentencePanel;
+                                  // NORTH is title
+            JPanel sentencePanel; // CENTER
+            JPanel buttonsPanel;  // SOUTH
+                JButton clearBtn;
+                JButton addAllBtn;
+        
+        // CENTER
+        JPanel wordStatsPanel;
+            JPanel statsPanel;
     
     Map<Pair<Integer, String>, List<Sentence>> chapters;
     
@@ -49,31 +57,57 @@ public class RightPanel extends JScrollPane {
         titlePanel.setLayout(new BorderLayout());
         titlePanel.add(title, BorderLayout.CENTER);
         
+        mainSentencePanel = new JPanel();
+        mainSentencePanel.setLayout(new BorderLayout());
+        mainSentencePanel.add(titlePanel, BorderLayout.NORTH);
+        
+        sentencePanel = new JPanel();
+        sentencePanel.add(new JLabel("content"));
+        sentencePanel.setLayout(new WrapLayout(WrapLayout.LEFT));
+        sentencePanel.setBackground(Utils.GRAY3);
+        mainSentencePanel.add(sentencePanel, BorderLayout.CENTER);
+        
+        buttonsPanel = new JPanel();
+        buttonsPanel.setBackground(Utils.GRAY3);
+        buttonsPanel.setLayout(new WrapLayout(WrapLayout.RIGHT, 10, 4));
+//        buttonsPanel.setBorder(new StrokeBorder(new BasicStroke(1)));
+        
+        clearBtn = new JButton("Clear");
+        addAllBtn = new JButton("Add all");
+        buttonsPanel.add(clearBtn);
+        buttonsPanel.add(addAllBtn);
+        
+        mainSentencePanel.add(buttonsPanel, BorderLayout.SOUTH);
+        
+        mainPanel.add(mainSentencePanel, BorderLayout.NORTH);
         
         
-        // SOUTH
+        
+        // CENTER
+        JPanel titlePanel2 = new JPanel();
+        titlePanel2.setLayout(new BorderLayout());
+        
         JLabel title2 = new JLabel(" Word and sentence statistics");
         title2.setPreferredSize(new Dimension(300, 27));
         title2.setOpaque(true);
         title2.setBackground(Utils.GRAY);
         title2.setFont(Utils.getFont(14));
         title2.setPreferredSize(new Dimension(title2.getPreferredSize().width, title2.getPreferredSize().height+5));
-    
-        JPanel titlePanel2 = new JPanel();
-        titlePanel2.setLayout(new BorderLayout());
-        titlePanel2.add(title2, BorderLayout.CENTER);
+        titlePanel2.add(title2, BorderLayout.NORTH);
         
         wordStatsPanel = new JPanel();
         wordStatsPanel.setLayout(new BorderLayout());
         wordStatsPanel.add(titlePanel2, BorderLayout.NORTH);
         
-        wordStatsContentPanel = new JPanel();
-        wordStatsContentPanel.setBackground(Utils.GRAY3);
-        wordStatsContentPanel.add(new JLabel("CONTENT"));
-        wordStatsPanel.add(wordStatsContentPanel, BorderLayout.CENTER);
+        statsPanel = new JPanel();
+        statsPanel.setBackground(Utils.GRAY3);
+        statsPanel.add(new JLabel("CONTENT"));
+        wordStatsPanel.add(statsPanel, BorderLayout.CENTER);
         
-        mainPanel.add(titlePanel, BorderLayout.NORTH);
-        mainPanel.add(wordStatsPanel, BorderLayout.SOUTH);
+        mainPanel.add(wordStatsPanel, BorderLayout.CENTER);
+        
+        
+        
         
         this.setViewportView(mainPanel);
     
@@ -89,41 +123,18 @@ public class RightPanel extends JScrollPane {
                 System.out.println(self.getSize());
                 
                 if (sentencePanel == null) return;
-    
+                
                 System.out.println("Pref.height=> " + sentencePanel.getLayout().preferredLayoutSize(sentencePanel).height);
                 
-                sentencePanel.setPreferredSize(new Dimension(self.getSize().width, sentencePanel.getLayout().preferredLayoutSize(sentencePanel).height));
+                sentencePanel.setPreferredSize(new Dimension(self.getSize().width-20, sentencePanel.getLayout().preferredLayoutSize(sentencePanel).height));
+//                sentencePanel.setPreferredSize(new Dimension(self.getSize().width, sentencePanel.getLayout().preferredLayoutSize(sentencePanel).height));
                 System.out.println("Pref.size=> " + sentencePanel.getSize());
                 sentencePanel.setMaximumSize(sentencePanel.getPreferredSize());
                 sentencePanel.setMinimumSize(sentencePanel.getPreferredSize());
                 sentencePanel.revalidate();
                 sentencePanel.doLayout();
-                
-//                var x = new Object() {int x = 0;};
-//                System.out.println(self.mainPanel.getComponents().length);
-//                Arrays.stream(self.mainPanel.getComponents()).forEach(c -> {
-//                    if (x.x <= 0)
-//                    System.out.println("width=>" + c.getWidth()+" : " +((JPanel)c).getLayout().preferredLayoutSize((JPanel)c).width);
-
-//                    Component c = ((JPanel)c1).getComponents()[1];
-
-//                    System.out.println(c.getName());
-//                    System.out.println(c.getPreferredSize().height);
-//                    System.out.println(((JPanel)c).getLayout().preferredLayoutSize((JPanel)c).height + " : " + ((JPanel)c).getLayout().minimumLayoutSize((JPanel)c).height);
-
-//                    ((JPanel)c).getLayout().preferredLayoutSize((JPanel)c).height;
-//                    ((JPanel)c).getLayout().minimumLayoutSize((JPanel)c).height;
-                
-                    // Crucial!
-                    //                     parentWidth - scrollBarWidth(arrox)  ,    preferredHeight of layout
-//                    c.setPreferredSize(new Dimension(self.getSize().width-25, ((JPanel)c).getLayout().preferredLayoutSize((JPanel)c).height));
-//                    c.setMaximumSize(c.getPreferredSize());
-//                    c.setMinimumSize(c.getPreferredSize());
-//                    c.revalidate();
-//                    c.doLayout();
-
-//                    x.x++;
-//                });
+                mainSentencePanel.revalidate();
+                mainSentencePanel.doLayout();
             }
             @Override public void componentMoved(ComponentEvent e) { }
             @Override public void componentShown(ComponentEvent e) { }
@@ -137,13 +148,15 @@ public class RightPanel extends JScrollPane {
     }
 
     public void onSentenceClick(Sentence clickedSentence) {
-//        sentencesPanel.removeAll();
-        if (sentencePanel != null) mainPanel.remove(sentencePanel);
+        sentencePanel.removeAll();
+//        if (sentencePanel != null) mainPanel.remove(sentencePanel);
+        
+        
         
         // SELECTED SENTENCE
-        this.sentencePanel = new JPanel();
-        sentencePanel.setLayout(new WrapLayout(WrapLayout.LEFT));
-        sentencePanel.setBackground(Utils.RED);
+//        this.sentencePanel = new JPanel();
+//        sentencePanel.setLayout(new WrapLayout(WrapLayout.LEFT));
+//        sentencePanel.setBackground(Utils.GRAY3);
 
         for (AbsWord word : clickedSentence.getWords()) {
             WordLabel lbl = new WordLabel(this, sentencePanel, word);
@@ -152,13 +165,21 @@ public class RightPanel extends JScrollPane {
             // (because listener overlapping problem)
             sentencePanel.removeMouseListener(lbl.containerParentlistener);
         }
-        mainPanel.add(sentencePanel, BorderLayout.CENTER);
+//        mainPanel.add(sentencePanel, BorderLayout.CENTER);
+        sentencePanel.setPreferredSize(new Dimension(this.getSize().width-20, sentencePanel.getLayout().preferredLayoutSize(sentencePanel).height));
+        System.out.println("Pref.size=> " + sentencePanel.getSize());
+        sentencePanel.setMaximumSize(sentencePanel.getPreferredSize());
+        sentencePanel.setMinimumSize(sentencePanel.getPreferredSize());
+        sentencePanel.revalidate();
+        sentencePanel.doLayout();
+        
+        
         
         // SENTENCE STATS
-        if (this.wordStatsContentPanel != null) wordStatsPanel.remove(wordStatsContentPanel);
+        if (this.statsPanel != null) wordStatsPanel.remove(statsPanel);
     
-        wordStatsContentPanel = new JPanel();
-        wordStatsContentPanel.setBackground(Utils.GRAY3);
+        statsPanel = new JPanel();
+        statsPanel.setBackground(Utils.GRAY3);
         JLabel stats = new JLabel("<html>"+clickedSentence.toString()
                 .replaceAll("<", "&lt;")
                 .replaceAll(">", "&gt;")
@@ -166,8 +187,8 @@ public class RightPanel extends JScrollPane {
                 "</html>");
         stats.setOpaque(false);
         System.out.println(stats.getText());
-        wordStatsContentPanel.add(stats);
-        wordStatsPanel.add(wordStatsContentPanel, BorderLayout.CENTER);
+        statsPanel.add(stats);
+        wordStatsPanel.add(statsPanel, BorderLayout.CENTER);
         
         
         // need to call this otherwise this components doesn't get updated
