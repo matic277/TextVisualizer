6da6b682c3ab632b1d32e0702c0750d3dc683504
matic277 <matic277@gmail.com>
2021-06-04 12:03:55 +0200

    Code improvement

--- src/main/java/panel/SlidingWindow.java
+++ src/main/java/panel/SlidingWindow.java
@@ -15,58 +15,43 @@ public class SlidingWindow extends Rectangle {
     final Color COLOR = Color.black;
     final Stroke STROKE = new BasicStroke(3f);
     
-    ChapterType currentChapterType = ChapterType.VERTICAL;
-    Renderer renderer = new VerticalRenderer();
+    ChapterType currentChapterType = ChapterType.HORIZONTAL;
+    Renderer renderer;
     
     public SlidingWindow(ChaptersPanel parent) {
         this.parent = parent;
         
+        this.renderer = VERTICAL_RENDERER;
         this.setBounds(30, 100, Utils.INITIAL_SLIDER_WIDTH, 90);
     }
     
-    public void onNewChapterTypeChange(ChapterType chapterType) {
-        currentChapterType = chapterType;
-        
-        if (chapterType == ChapterType.HORIZONTAL) renderer = new VerticalRenderer();
-        else if (chapterType == ChapterType.VERTICAL) renderer = new HorizontalRenderer();
-        
-        this.setLocation(10, 10);
-        this.parent.repaint();
-    }
-    
+    private final Renderer HORIZONTAL_RENDERER = (gr, slider) -> {
+        Renderer.drawBaseRectangle(gr, slider);
+        Renderer.enableAntiAliasing(gr);
+        // left
+        gr.fillRoundRect(x-2, y+1, 11, height-3, 5, 5);
+        // right
+        gr.fillRoundRect(x+width, y+1,  5, height-3, 5, 5);
+    };
+    private final Renderer VERTICAL_RENDERER = (gr, slider) -> {
+        Renderer.drawBaseRectangle(gr, slider);
+        Renderer.enableAntiAliasing(gr);
+        // top
+        gr.fillRoundRect(x, y, width+1, 11, 5, 5);
+        // bottom
+        gr.fillRoundRect(x, y+height-5,  width+1, 5, 5, 5);
+    };
     private interface Renderer {
         void draw(Graphics2D g, SlidingWindow slider);
-        default void drawBaseRectangle(Graphics2D gr, SlidingWindow slider) {
+        static void drawBaseRectangle(Graphics2D gr, SlidingWindow slider) {
             gr.setColor(slider.COLOR);
             // lines, draw before antialiasing!
             gr.setStroke(slider.STROKE);
             gr.drawRect(slider.x+1, slider.y+2, slider.width-2, slider.height-6);
         }
-        default void enableAntiAliasing(Graphics2D gr) {
+        static void enableAntiAliasing(Graphics2D gr) {
             gr.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
             gr.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        }
-    }
-    private class HorizontalRenderer implements Renderer {
-        @Override public void draw(Graphics2D gr, SlidingWindow slider) {
-            drawBaseRectangle(gr, slider);
-            enableAntiAliasing(gr);
-            
-            // left
-            gr.fillRoundRect(x-2, y+1, 11, height-3, 5, 5);
-            // right
-            gr.fillRoundRect(x+width, y+1,  5, height-3, 5, 5);
-        }
-    }
-    private class VerticalRenderer implements Renderer {
-        @Override public void draw(Graphics2D gr, SlidingWindow slider) {
-            drawBaseRectangle(gr, slider);
-            enableAntiAliasing(gr);
-            
-            // top
-            gr.fillRoundRect(x, y, width+1, 11, 5, 5);
-            // bottom
-            gr.fillRoundRect(x, y+height-5,  width+1, 5, 5, 5);
         }
     }
     
@@ -75,6 +60,16 @@ public class SlidingWindow extends Rectangle {
         gr.setColor(COLOR);
         
         renderer.draw(gr, this);
+    }
+    
+    public void onNewChapterTypeChange(ChapterType chapterType) {
+        currentChapterType = chapterType;
+        
+        if (chapterType == ChapterType.HORIZONTAL) renderer = VERTICAL_RENDERER;
+        else if (chapterType == ChapterType.VERTICAL) renderer = HORIZONTAL_RENDERER;
+        
+        this.setLocation(10, 10);
+        this.parent.repaint();
     }
     
     // Also highlights/unhighlights hovered sentences
